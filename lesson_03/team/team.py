61986e43d462a028c760cf7745f5f30ea0b48cc9
Jessen Forbush <for21011@byui.edu>
2025-05-12 13:40:35 -0600

    update

--- lesson_03/team/team.py
+++ lesson_03/team/team.py
@@ -2,7 +2,7 @@
 Course: CSE 351 
 Lesson: L03 team activity
 File:   team.py
-Author: <Add name here>
+Author: <Jessen Forbush>
 
 Purpose: Retrieve Star Wars details from a server
 
@@ -34,6 +34,7 @@ TODO
 
 from datetime import datetime, timedelta
 import threading
+import queue
 from common import *
 
 # Include cse 351 common Python files
@@ -42,7 +43,16 @@ from cse351 import *
 # global
 call_count = 0
 
-def get_urls(film6, kind):
+
+def thread_function(q):
+    while True:
+        url = q.get()
+        if (url == None):
+            break
+        item = get_data_from_server(url)
+        print(f'  - {item['name']}')
+
+def get_urls(film6, kind,):
     global call_count
 
     urls = film6[kind]
@@ -51,7 +61,7 @@ def get_urls(film6, kind):
         call_count += 1
         item = get_data_from_server(url)
         print(f'  - {item['name']}')
-
+        
 def main():
     global call_count
 
@@ -61,14 +71,42 @@ def main():
     film6 = get_data_from_server(f'{TOP_API_URL}/films/6')
     call_count += 1
     print_dict(film6)
-
+    
+    # Create a queue that can be shared
+    q = queue.Queue()
+    
+    # Create threads
+    threads = []
+    thread_count = 95
+    for i in range (thread_count):
+        t = threading.Thread(target=thread_function , args=(q,))
+        threads.append(t)
+    
+    for url in film6['characters']:
+        q.put(url)
+    
+    for url in film6['planets']:
+        q.put(url)
+        
+    for url in film6['starships']:
+        q.put(url)
+        
+    for url in film6['vehicles']:
+        q.put(url)
+        
+    for url in film6['species']:
+        q.put(url)
+        
+    for i in range(thread_count):
+        q.put(None)
+        
+    for t in threads:
+        t.start()
+        
+    for t in threads:
+        t.join()
+    
     # Retrieve people
-    get_urls(film6, 'characters')
-    get_urls(film6, 'planets')
-    get_urls(film6, 'starships')
-    get_urls(film6, 'vehicles')
-    get_urls(film6, 'species')
-
     log.stop_timer('Total Time To complete')
     log.write(f'There were {call_count} calls to the server')
 
